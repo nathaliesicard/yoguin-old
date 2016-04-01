7ecbbdb25340fac457dd877c301e833de2b83a26
nathaliesicard <nathalie.sicard@gmail.com>
2016-04-01 12:20:04 -0600

    Added kia-range

--- server.js
+++ server.js
@@ -2,10 +2,13 @@
  * Created by nathaliesicard on 3/28/16.
  */
 var koa = require('koa');
+var range = require('koa-range');
 var serve =require('koa-better-static');
 
 
+
 var app = koa();
+app.use(range);
 app.use(serve('dist', {index: 'index.html'}));
 
 var port = process.env.PORT || 8888;
