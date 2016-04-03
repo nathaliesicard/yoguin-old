24a4ccbe4a0916aa5aace2aa958214416b81d82c
nathaliesicard <nathalie.sicard@gmail.com>
2016-04-03 18:10:20 -0500

    Styling of home with screenType

--- app/components/Home.js
+++ app/components/Home.js
@@ -8,25 +8,49 @@ var MainContainer = require('../containers/MainContainer');
 var MeditateButton= require('./MeditateButton');
 var color = require('color');
 var Radium = require('radium');
+var ScreenTypeMixin = require('./../mixins/ScreenTypeMixin');
 
-var styles = {
-  homepageBrand: {
-    background: 'transparent',
-    fontFamily: 'Cookie, cursive',
-    fontSize: '6em',
-    fontWeight: 900,
-    color: '#fff'
-  },
-  homepageDesc: {
-    fontFamily: 'Raleway, sans-serif',
-    fontWeight: 400,
-    color: '#fff',
-    fontSize: '3em'
-  }
-};
+
 
 var Home = React.createClass({
+  mixins: [ ScreenTypeMixin ],
   render: function() {
+
+    if (this.state.screenType =='DESKTOP') {
+      var styles = {
+        homepageBrand: {
+          background: 'transparent',
+          fontFamily: 'Cookie, cursive',
+          fontSize: '6em',
+          fontWeight: 900,
+          color: '#fff'
+        },
+        homepageDesc: {
+          fontFamily: 'Raleway, sans-serif',
+          fontWeight: 400,
+          color: '#fff',
+          fontSize: '1.5em'
+        }
+      };
+    } else {
+      var styles = {
+        homepageBrand: {
+          background: 'transparent',
+          fontFamily: 'Cookie, cursive',
+          fontSize: '7em',
+          fontWeight: 900,
+          color: '#fff'
+        },
+        homepageDesc: {
+          fontFamily: 'Raleway, sans-serif',
+          fontWeight: 500,
+          color: '#fff',
+          fontSize: '3.5em'
+        }
+      }
+
+    }
+
     return (
       <MainContainer>
         <h1 style={styles.homepageBrand}>Yoguin</h1>
@@ -37,4 +61,4 @@ var Home = React.createClass({
   }
 });
 
-module.exports = Home;
+module.exports = Radium(Home);
--- app/components/Main.js
+++ app/components/Main.js
@@ -4,9 +4,7 @@
 var React = require('react');
 var ScreenTypeMixin = require('./../mixins/ScreenTypeMixin');
 const ReactDriveIn = require("react-drive-in");
-
 var bgImgSrc = require('../assets/images/bg.jpg');
-
 var bgVideoSrc = require('../assets/videos/video.mp4');
 var BgVideo = require('./BgVideo');
 
