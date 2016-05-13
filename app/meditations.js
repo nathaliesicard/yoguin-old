5dd5c97b95442476a32438e9811303bb8c037df5
nathaliesicard <nathalie.sicard@gmail.com>
2016-05-13 16:47:50 -0500

    Changed path of meditations.js

--- app/meditations.js
+++ app/meditations.js
@@ -3,50 +3,50 @@ module.exports = {
     name: 'Aprendiendo a Meditar 01',
     quip: 'Enfoque en la Respiración',
     duration: 10,
-    url: "/audio/aprendiendo-01.mp3",
-    img: "/img/meditation-mountains.jpg"
+    url: "audio/aprendiendo-01.mp3",
+    img: "img/meditation-mountains.jpg"
   },
   'Aprendiendo a Meditar 02': {
     name: 'Aprendiendo a Meditar 02',
     quip: 'Concéntrate con Notas Mentales',
     duration: 10,
-    url: "/audio/aprendiendo-02.mp3",
-    img: "/img/meditation-mountains.jpg"
+    url: "audio/aprendiendo-02.mp3",
+    img: "img/meditation-mountains.jpg"
   },
   'Aprendiendo a Meditar 03': {
     name: 'Aprendiendo a Meditar 03',
     quip: 'Escanea tu Cuerpo',
     duration: 10,
-    url: "/audio/aprendiendo-03.mp3",
-    img: "/img/meditation-mountains.jpg"
+    url: "audio/aprendiendo-03.mp3",
+    img: "img/meditation-mountains.jpg"
   },
   'Aprendiendo a Meditar 04': {
     name: 'Aprendiendo a Meditar 04',
     quip: 'Salir de Piloto Automático',
     duration: 11,
-    url: "/audio/aprendiendo-04.mp3",
-    img: "/img/meditation-mountains.jpg"
+    url: "audio/aprendiendo-04.mp3",
+    img: "img/meditation-mountains.jpg"
   },
   'Aprendiendo a Meditar 05': {
     name: 'Aprendiendo a Meditar 05',
     quip: 'Haz una Pausa',
     duration: 11,
-    url: "/audio/aprendiendo-05.mp3",
-    img: "/img/meditation-mountains.jpg"
+    url: "audio/aprendiendo-05.mp3",
+    img: "img/meditation-mountains.jpg"
   },
   'Aprendiendo a Meditar 06': {
     name: 'Aprendiendo a Meditar 06',
     quip: 'El Valor de la Paciencia',
     duration: 10,
-    url: "/audio/aprendiendo-06.mp3",
-    img: "/img/meditation-mountains.jpg"
+    url: "audio/aprendiendo-06.mp3",
+    img: "img/meditation-mountains.jpg"
   },
   'Aprendiendo a Meditar 07': {
   name: 'Aprendiendo a Meditar 07',
   quip: 'La Consciencia',
   duration: 11,
-  url: "/audio/aprendiendo-07.mp3",
-  img: "/img/meditation-mountains.jpg"
+  url: "audio/aprendiendo-07.mp3",
+  img: "img/meditation-mountains.jpg"
 }
 
 };
